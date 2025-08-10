af68a2912750aaf842f31b0ab4e5b8cd2049e68d
işyar yılmaz <isyaryilmaz2121@gmail.com>
2025-08-10 14:50:56 +0300

    Add files via upload

---
+++ Migrations/20250801144810_AddOrders.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStoreMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
